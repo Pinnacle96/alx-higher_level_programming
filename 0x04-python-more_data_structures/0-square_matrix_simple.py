dbc729b129adbbf2fc6d7a558995824f23038a3b
Pinnacle96 <noah.poeni@gmail.com>
2022-12-07 10:06:41 -0800

    modify

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-def squared(element):
-    return (element**2)
-
-
 def square_matrix_simple(matrix=[]):
-    new = matrix[:]
-    for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                new[i] = list(map(squared, matrix[i]))
-    return new
+    dup_matrix = matrix.copy()
+    for line in range(len(matrix)):
+        dup_matrix[line] = list(map((lambda x: x**2), matrix[line]))
+    return(dup_matrix)
